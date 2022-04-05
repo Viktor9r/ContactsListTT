b39166ad1da3cd1227c5809b85da7e9e5368c3c0
Viktor Riabokon <vikrib9@gmail.com>
2022-04-05 13:23:55 +0300

    sol without correcting info

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,17 @@
 import React from 'react';
 import './App.scss';
-
-interface Props {
-  onClick: () => void;
-}
-
-export const Provider: React.FC<Props> = React.memo(
-  ({ onClick, children }) => (
-    <button
-      type="button"
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  ),
-);
+import contactsFromServer from './api/contacts.json';
+import { ContactsList } from './components/ContactsList/ContactsList';
 
 export const App: React.FC = () => {
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>
-        <TodoList />
-      </Provider>
+    <div className="body">
+      <div className="title">Contacts List</div>
+      <div className="page">
+        <div className="page-content">
+          <ContactsList contactsFromServer={[...contactsFromServer]} />
+        </div>
+      </div>
     </div>
   );
 };
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="react-scripts" />
+type Status = 'work' | 'family' | 'friends' | 'others';
+
+type Contact = {
+  id: number
+  name: string,
+  phone: string,
+  info: string,
+  group: string,
+};
+
+interface ContactNew extends Contact {
+  [key: string]: string | number;
+}
